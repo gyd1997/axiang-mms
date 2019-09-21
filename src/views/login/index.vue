bd69168cbdefd85230e10d739cd10a25c1829bf2
gyd1997 <gyd0008@163.com>
2019-09-21 18:13:50 +0800

    实现登录

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,33 +1,74 @@
 <template>
   <div class="login-container">
-    <el-form ref="form" :model="form" label-width="80px" class="login-form">
+    <el-form ref="form" :model="form" :rules="rules" label-width="80px" class="login-form">
       <h2 class="login-title">阿香会员管理系统</h2>
-      <el-form-item label="账号">
+      <el-form-item label="账号" prop="username">
         <el-input v-model="form.username"></el-input>
       </el-form-item>
-      <el-form-item label="密码">
+      <el-form-item label="密码" prop="password">
         <el-input v-model="form.password" type="password"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">登录</el-button>
+        <el-button type="primary" @click="submitForm('form')">登录</el-button>
       </el-form-item>
     </el-form>
   </div>
 </template>
 
 <script>
+  import { login, getUserInfo } from '../../api/login'
   export default {
     data() {
       return {
         form: {
           username: '',
-          password: '',
+          password: ''
+        },
+        rules: {
+          username: [
+            {required: true, message: '账号不能为空', trigger: 'blur'}
+          ],
+          password: [
+            {required: true, message: '密码不能为空', trigger: 'blur'}
+          ]
         }
       }
     },
     methods: {
-      onSubmit() {
-        console.log('submit!');
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            login(this.form.username, this.form.password).then(response => {
+              const resp = response.data
+              if (resp.flag) {
+                // 验证成功, 通过token去获取用户信息
+                getUserInfo(resp.data.token).then(response => {
+                  // 获取到了用户的数据
+                  const respUser = response.data
+                  if (respUser.flag) {
+                    console.log('userInfo', respUser.data)
+                    // 1. 保存 token, 用户信息
+                    localStorage.setItem('ax-mms-user', JSON.stringify(respUser.data))
+                    localStorage.setItem('ax-mms-token', resp.data.token)
+                    // 2. 前往首页
+                    this.$router.push('/')
+                  } else {
+                    this.$message({
+                      message: respUser.message,
+                      type: 'warning'
+                    });
+                  }
+                })
+              } else {
+                // 未通过, 弹出警告
+                this.$message.error('错了哦，这是一条错误消息');
+              }
+            })
+          } else {
+            console.log('error submit!!')
+            return false
+          }
+        })
       }
     }
   }
