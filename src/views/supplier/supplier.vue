dd03bc49e8d79c301249c5ec48aa513a0527d770
gyd1997 <gyd0008@163.com>
2019-10-02 22:47:48 +0800

    供应商列表实现

--- src/views/supplier/supplier.vue
+++ src/views/supplier/supplier.vue
@@ -1,5 +1,55 @@
 <template>
   <div>
-    供应商管理
+    <el-table
+      :data="list"
+      height="380"
+      border
+      style="width: 100%">
+      <el-table-column type="index" label="序号"></el-table-column>
+      <el-table-column prop="name" label="供应商名称"></el-table-column>
+      <el-table-column prop="linkman" label="联系人" width="90"></el-table-column>
+      <el-table-column prop="mobile" label="联系电话"></el-table-column>
+      <el-table-column prop="remark" label="备注"></el-table-column>
+      <el-table-column label="操作" width="150">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            @click="handleEdit(scope.row.id)">编辑</el-button>
+          <el-button
+            size="mini"
+            type="danger"
+            @click="handleDelete(scope.row.id)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
+
+<script>
+import supplierApi from '../../api/supplier'
+
+export default {
+  data () {
+    return {
+      list: []
+    }
+  },
+  created () {
+    this.fetchList()
+  },
+  methods: {
+    fetchList () {
+      supplierApi.getList().then(response => {
+        this.list = response.data.data
+        console.log(this.list)
+      })
+    },
+    handleEdit (id) {
+      console.log('编辑', id)
+    },
+    handleDelete (id) {
+      console.log('删除', id)
+    }
+  }
+}
+</script>
