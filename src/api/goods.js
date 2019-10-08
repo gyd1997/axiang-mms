f6dc076910bf7bc6b79a9c90efa5fd5ed71bba7e
gyd1997 <gyd0008@163.com>
2019-10-08 14:57:34 +0800

    商品列表

---
+++ src/api/goods.js
@@ -0,0 +1,12 @@
+import request from '../utils/request'
+
+export default {
+  // 分页条件查询
+  search (page, size, searchMap) {
+    return request({
+      url: `/goods/list/search/${page}/${size}`,
+      method: 'post',
+      data: searchMap
+    })
+  }
+}
--- src/views/goods/goods.vue
+++ src/views/goods/goods.vue
@@ -1,5 +1,81 @@
 <template>
   <div>
-    商品管理
+    <el-table
+      :data="list"
+      height="380"
+      border
+      style="width: 100%">
+      <el-table-column type="index" label="序号"></el-table-column>
+      <el-table-column prop="name" label="商品名称"></el-table-column>
+      <el-table-column prop="code" label="商品编码" width="90"></el-table-column>
+      <el-table-column prop="spec" label="商品规格"></el-table-column>
+      <el-table-column prop="retailPrice" label="零售价"></el-table-column>
+      <el-table-column prop="purchasePrice" label="进货价"></el-table-column>
+      <el-table-column prop="storageNum" label="库存数量"></el-table-column>
+      <el-table-column prop="supplierName" label="供应商"></el-table-column>
+      <el-table-column label="操作" width="150">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            @click="handleEdit(scope.row.id)">编辑</el-button>
+          <el-button
+            size="mini"
+            type="danger"
+            @click="handleDelete(scope.row.id)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage"
+      :page-sizes="[10, 20, 50]"
+      :page-size="pageSize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total">
+    </el-pagination>
   </div>
 </template>
+
+<script>
+import goodsApi from '../../api/goods'
+export default {
+  data () {
+    return {
+      list: [],
+      currentPage: 1,
+      pageSize: 10,
+      total: 0,
+      searchMap: {}
+    }
+  },
+  created () {
+    this.fetchList()
+  },
+  methods: {
+    fetchList () {
+      goodsApi.search(this.currentPage, this.pageSize, this.searchMap).then(response => {
+        const data = response.data.data
+        this.list = data.rows
+        this.total = data.total
+        console.log(this.list)
+      })
+    },
+    handleEdit () {
+
+    },
+    handleDelete () {
+
+    },
+    handleSizeChange (val) {
+      this.pageSize = val
+      this.fetchList()
+    },
+    handleCurrentChange () {
+      this.currentPage = val
+      this.fetchList()
+    }
+  }
+}
+</script>
