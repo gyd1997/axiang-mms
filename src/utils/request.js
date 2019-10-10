f9e93e8849f602ec2867a746302a402f3cf3146d
gyd1997 <gyd0008@163.com>
2019-10-10 17:59:28 +0800

    loading实现与异常处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,4 +1,25 @@
 import axios from "axios"
+import { Loading, Message } from 'element-ui'
+
+const loading = {
+  loadingInstance: null,
+  open () {
+    if (this.loadingInstance === null) {
+      this.loadingInstance = Loading.service({
+        target: '.main',
+        text: '拼命加载中...',
+        background: 'rgba(0, 0, 0, .5)'
+      })
+    }
+  },
+  close () {
+    // 不为空时, 则关闭加载窗口
+    if (this.loadingInstance !== null) {
+      this.loadingInstance.close()
+    }
+    this.loadingInstance = null
+  }
+}
 
 const request = axios.create({
   baseURL: process.env.VUE_APP_BASE_API,
@@ -6,16 +27,32 @@ const request = axios.create({
 })
 
 request.interceptors.request.use(config => {
-    // 请求拦截
+  // 请求拦截
+  loading.open()
   return config
 }, error => {
+  loading.close()
   return Promise.reject(error)
 })
 
 request.interceptors.response.use(response => {
-  // response.data
+  loading.close()
+  const resp = response.data
+  if (resp.code !== 2000) {
+    Message({
+      message: resp.message || '哦吼, 加载失败了!',
+      type: 'warning',
+      duration: 5000
+    })
+  }
   return response
 }, error => {
+  loading.close()
+  Message({
+    message: error.message,
+    type: 'error',
+    duration: 5000
+  })
   return Promise.reject(error)
 })
 
